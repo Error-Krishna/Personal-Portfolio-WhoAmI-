1bbb34cd95f347e2bb48cfd8fed1c004a6166a54
KRISHNA GOYAL <krishnagoyal@KRISHNAs-MacBook-Air-2.local>
2025-05-25 14:17:21 +0530

    Test commit from VS Code terminal

--- WhoAmI/WhoAmI/settings.py
+++ WhoAmI/WhoAmI/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-8hldt515t&7hdepw4su_5l-e@q)ghryy@kiwzth3to_@sx25*9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
